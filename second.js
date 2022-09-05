755d8dde892b864c64eeb769dcda83e45dbbca24
James-Frown <21122@ctca.co.za>
2022-09-05 09:17:31 +0200

    Changing up some other html
    making changes to the second html page and js

--- second.js
+++ second.js
@@ -1,73 +1,109 @@
-var totalUsers = 0;
+let xData = [];
 
-var currentDay;
+let yData = [];
 
-function addRange(input, target) {
-    totalUsers = input;
-    target.innerHTML = input.value;
-    input.value = "0";
-}
+let randData = [];
 
-function clearRange(input, target) {
-    totalUsers = 0;
-    input.value = "0";
-    target.innerHTML = input.value;
-}
-
-function weeklyOut(outPut2) {
-    outPut2.innerHTML = "weekly";
-}
-
-function monthlyOut(outPut2) {
-    outPut2.innerHTML = "Monthly";
-}
-
-function getCurDay() {
-    let currentDay = "";
-
+function getDay(target) {
+    let day;
     switch (new Date().getDay()) {
         case 0:
-            currentDay = "Sunday";
+            day = "Sunday";
             break;
         case 1:
-            currentDay = "Monday";
+            day = "Monday";
             break;
         case 2:
-            currentDay = "Tuesday";
+            day = "Tuesday";
             break;
         case 3:
-            currentDay = "Wednesday";
+            day = "Wednesday";
             break;
         case 4:
-            currentDay = "Thursday";
+            day = "Thursday";
             break;
         case 5:
-            currentDay = "Friday";
+            day = "Friday";
             break;
         case 6:
-            currentDay = "Saturday";
-            break;
-        case Error:
-            currentDay = "Un-Avaliable";
+            day = "Saturday";
     }
-
-    return currentDay;
+    target.innerHTML = day;
 }
 
-function buildChart(userChartVar) {
+function clearData(target) {
+    target.innerHTML = "";
+    randData = [];
+}
 
-    let currDay = getCurDay();
-
-    let xValues = [];
-
-    if (weekly is selected) {
-        xValues = [1, 2, 3, 4, 5, 6, 7];
-    } else
-    if (monthly is selected) {
-        for (let x = o; x < 31; x++) {
-            xValues.push(x);
-        }
+function getYRange(input, target) {
+    for (let i = 0; i <= input; i++) {
+        yData.push(i);
+        console.log(yData);
     }
+    target.innerHTML = input.value;
+    input.value = "0";
+    return;
+}
+
+function getXRange(input, target) {
+    for (let i = 0; i <= input; i++) {
+        xData.push(i);
+        console.log(xData);
+    }
+    target.innerHTML = input.value;
+    input.value = "0";
+    return;
+}
+
+function getRandomData(var1, Var2, target) {
+    let randNum;
+    for (let i = 0; i <= var1; i++) {
+        randNum = Math.floor(Math.random() * Var2);
+        console.log(randNum);
+        if (i === 0) {
+            target.innerHTML += randNum;
+        } else {
+            target.innerHTML += ", " + randNum;
+        }
+        randData.push(randNum);
+        console.log(randData);
+    }
+    return;
+}
+
+function templateGraph(chartVar) {
+
+    var xValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+    var yValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+    var dataValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    new Chart(chartVar, {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: dataValues
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: yValues[0], max: yValues[yValues.length - 1] } }],
+            }
+        }
+    });
+}
+
+function popViz(varX, varY, varData, varChart) {
+
+    var xValues = [];
 
     var yValues = [];
 
@@ -88,7 +124,7 @@ function buildChart(userChartVar) {
             datasets: [{
                 fill: false,
                 lineTension: 0,
-                backgroundColor: "rgba(0,52,255,1.0)",
+                backgroundColor: "rgba(0,0,255,1.0)",
                 borderColor: "rgba(0,0,255,0.1)",
                 data: dataValues
             }]
@@ -100,5 +136,106 @@ function buildChart(userChartVar) {
             }
         }
     });
+
+}
+
+function clearViz(varX, varY, chartVar) {
+    var xValues = [];
+
+    var yValues = [];
+
+    for (let i = 0; i <= varX; i++) {
+        xValues.push(i);
+    }
+
+    for (let m = 0; m <= varY; m++) {
+        yValues.push(m);
+    }
+
+    let dataValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    new Chart(chartVar, {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: dataValues
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: yValues[0], max: yValues[yValues.length - 1] } }],
+            }
+        }
+    });
+
+    return dataValues;
+}
+
+const daysInWeek = 7;
+const daysInMonth = 31;
+
+function weeklySelected() {
+    let dataValues = [];
+
+    for (let m = 1; m < daysInWeek + 1; m++) {
+        dataValues.push(m);
+    }
+
+    new Chart("Weekly", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: dataValues
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: yValues[0], max: yValues[yValues.length - 1] } }],
+            }
+        }
+    });
+}
+
+function monthlySelected() {
+    let dataValues = [];
+
+    for (let m = 1; m < daysInMonth + 1; m++) {
+        dataValues.push(m);
+    }
+
+    new Chart("Monthly", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: dataValues
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: yValues[0], max: yValues[yValues.length - 1] } }],
+            }
+        }
+    });
+}
+
+function showAdmin() {
 
 }
